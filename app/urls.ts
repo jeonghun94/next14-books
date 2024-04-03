1b7afe86852e77c854e09cbc0caf0c4ab7f2eaf9
조정훈 <jeongh1021@naver.com>
2024-04-03 21:44:30 +0900

    test2

--- app/urls.ts
+++ app/urls.ts
@@ -1,5 +1,7 @@
 export const GET_BOOKS_API_URL =
   "https://books-api.nomadcoders.workers.dev/lists";
 
-export const GET_BOOK_API_URL2 =
+export const GET_BOOK_API_URL =
   "https://books-api.nomadcoders.workers.dev/list?name=";
+
+export const GET_BOOKS_BY_CATEGORY_API_URL = "";
